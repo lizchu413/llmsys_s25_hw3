50add7d60a6f06e325a99adf1d2f83beaca4c120
lizchu413 <lizchu413@gmail.com>
2025-03-11 14:16:08 -0400

    yo

--- minitorch/cuda_kernel_ops.py
+++ minitorch/cuda_kernel_ops.py
@@ -408,7 +408,26 @@ class CudaKernelOps(TensorOps):
       #   BEGIN ASSIGN3_1
       print(f"out_grad.shape: {out_grad.shape}")
       print(f"soft_inp.shape: {soft_inp.shape}")
+      batch_size, nhead, from_len, to_len = soft_inp.shape
+      rows = batch_size * nhead * from_len
       stream_1 = torch.cuda.current_stream().cuda_stream
+
+      lib_softmax.launch_attn_softmax_bw.argtypes = [
+        np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags='C_CONTIGUOUS'),
+        np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags='C_CONTIGUOUS'),
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_void_p
+      ]
+      lib_softmax.launch_attn_softmax_bw.restype = None
+
+      lib_softmax.launch_attn_softmax_bw(
+        out_grad._tensor._storage,
+        soft_inp._tensor._storage,
+        rows,
+        to_len,
+        stream_1
+      )
 
       return out_grad
       #   END ASSIGN3_1
